63b74dcfaf382acf2c2d6d5ede66d7dbfb77cc40
chanu <cw8881@naver.com>
2023-06-13 01:58:06 +0900

    feat : 데이트 일정 화면 완료

---
+++ app/src/main/java/com/example/petsapce_week1/calendar/CalendarSecondAdapter.kt
@@ -0,0 +1,75 @@
+package com.example.petsapce_week1.calendar
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.example.petsapce_week1.R
+import com.example.petsapce_week1.databinding.CalendarItemBinding
+import com.example.petsapce_week1.databinding.DatingRowBinding
+import com.example.petsapce_week1.home.Home2MainData
+
+class CalendarSecondAdapter(var items: ArrayList<CalendarSecondData>) :
+    RecyclerView.Adapter<CalendarSecondAdapter.ViewHolder>() {
+
+
+    fun moveItem(oldPos: Int, newPos: Int) {
+        val item = items[oldPos]
+        items.removeAt(oldPos)
+        items.add(newPos, item)
+        notifyItemMoved(oldPos, newPos)
+    }
+
+    fun removeItem(pos: Int) {
+        items.removeAt(pos)
+        notifyItemRemoved(pos)
+    }
+
+    interface OnItemClickListener {
+        fun OnItemClick(data: CalendarMainData)
+//        fun onClick(v: View, position: Int)
+    }
+
+
+    var itemClickListener: OnItemClickListener? = null //초기값 null값
+
+
+    inner class ViewHolder(val binding: DatingRowBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        @SuppressLint("SetTextI18n")
+        fun bind(data: CalendarSecondData) {
+
+            binding.apply {
+                textNumber.text = data.number.toString()
+                textDate.text = data.text
+            }
+
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding =
+            DatingRowBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+
+        holder.bind(items[position])
+
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+
+}
+
+
+
